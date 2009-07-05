1ef86f0d39672a57eef35b04aece43dd2691b741
Ramarren <ramarren@gmail.com>
2009-07-05 11:27:03 +0200

    Export examples.

--- examples/package.lisp
+++ examples/package.lisp
@@ -1,2 +1,14 @@
 (defpackage #:clutter-examples
-    (:use #:cl #:iterate #:alexandria #:cffi-clutter #:cffi))
+    (:use #:cl #:iterate #:alexandria #:cffi-clutter #:cffi)
+  (:export #:chapter-4
+           #:chapter-5-1
+           #:chapter-5-2
+           #:chapter-5-3
+           #:chapter-5-4
+           #:chapter-6-1
+           #:chapter-6-2
+           #:chapter-7
+           #:chapter-8
+           #:chapter-9
+           #:triangle-example
+           #:subclass-example))
